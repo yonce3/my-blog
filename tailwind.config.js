cb57389ad2b948123f772711f1e54712337c50c8
yonce3 <k0914t_u@icloud.com>
2022-10-01 20:16:51 +0900

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
