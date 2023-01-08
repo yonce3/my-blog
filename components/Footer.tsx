5aa5f83c7e71609e355edca0fdde1f1331fdb1da
yonce3 <k0914t_u@icloud.com>
2023-01-08 16:48:02 +0900

    fix footer github icon path

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Footer = (): JSX.Element => {
   return (
     <footer className="bg-blue-500 text-white">
-      <div className="flex justify-center mx-6 items-center h-16">
-        <img src="public/GitHub-Mark.png" />
+      <div
+        className="flex justify-center mx-6 items-center h-16 cursor-pointer"
+        onClick={() => {
+          window.location.href = "https://github.com/yonce3";
+        }}
+      >
+        <Image
+          src="/../public/github-mark.png"
+          alt="picute of github"
+          width={30}
+          height={30}
+        />
       </div>
       <span>&copy; taro</span>
     </footer>
